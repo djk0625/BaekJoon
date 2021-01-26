5363e764bc04a10497a6e43b5f92e88e3688da5b
djk0625 <djk0625@naver.com>
2021-01-26 21:07:13 +0900

    2021-01-26

---
+++ Class 2/1259.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class G {
+    public static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args)
+    {
+        while(true)
+        {
+            
+        String n = scanner.nextLine();
+        int t = n.length();
+        String m = "yes";
+
+            for(int i = 0; i < t/2; i++)
+            {
+                if(n.charAt(i) != n.charAt(t - i - 1))
+                {
+                    m = "no";                    
+                }
+            }
+
+            if(n.equals("0"))
+            {
+                break;
+            }
+            System.out.println(m);
+        }
+    }
+}
